3ddb137023340985d99d85a8866e15b07717569c
parinovra <43321097+parinovra@users.noreply.github.com>
2020-10-29 01:51:47 +0300

    Переписал код для Задачия 2 на использование объекта

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -12,10 +12,26 @@ public class MyFirstProgram {
 
 		Rectangle r = new Rectangle(4, 6);
 		System.out.println("Площадь прямоугольника со сторонами "  + r.a + " и " + r.b + " = " + r.area());
+
+		Point p1 = new Point();
+		p1.x = 1;
+		p1.y = 3;
+
+		Point p2 = new Point();
+		p2.x = 2;
+		p2.y = 4;
+
+//		double dx = Math.abs(p2.x - p1.x);
+//		double dy = Math.abs(p2.y - p1.y);
+
+		System.out.println("Расстояние между двумя точками (" + p1.x + ", " + p1.y + ") и (" + p2.x + ", " + p2.y + ") = " + distance(p1, p2));
 	}
 
 	public static void hello(String somebody) {
 		System.out.println("Hello " + somebody + "!");
 	}
 
+	public static double distance(Point p1, Point p2) {
+		return Math.sqrt(Math.pow((Math.abs(p2.x - p1.x)), 2) + Math.pow((Math.abs(p2.y - p1.y)), 2));
+	}
 }
