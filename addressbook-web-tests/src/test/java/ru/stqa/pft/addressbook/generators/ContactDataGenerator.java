8f66082969ced551060f5c48321ec4d6a0b38799
parinovra <43321097+parinovra@users.noreply.github.com>
2020-12-02 01:52:25 +0300

    Создан ContactDataGenerator

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -1,5 +1,63 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContactDataGenerator {
 
+    @Parameter(names = "-c", description = "Contact count")
+    public int count;
+
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+
+    public static void main(String[] args) throws IOException {
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+        generator.run();
+    }
+
+    private void run() throws IOException {
+        List<ContactData> contacts = generateContacts(count);
+        save(contacts, new File(file));
+    }
+
+    private void save(List<ContactData> contacts, File file) throws IOException {
+        System.out.println(new File(".").getAbsolutePath());
+        Writer writer = new FileWriter(file);
+        for (ContactData contact : contacts) {
+            writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getFirstName(), contact.getLastName(),
+                    contact.getAddress(), contact.getHome(), contact.getMobile(), contact.getWork(),
+                    contact.getEmail(), contact.getEmail2(), contact.getEmail3())); //группу добавить?
+        }
+        writer.close();
+    }
+
+    private List<ContactData> generateContacts(int count) {
+        List<ContactData> contacts = new ArrayList<ContactData>();
+        for (int i = 0; i < count; i++) {
+            contacts.add(new ContactData().withFirstName(String.format("Петя%s", i))
+                    .withLastName(String.format("Петров%s", i)).withAddress(String.format("Москва%s", i))
+                    .withHome(String.format("8495012345%s", i)).withMobile(String.format("890099999%s", i))
+                    .withWork(String.format("849954321%s", i)).withEmail(String.format("email1.%s@test.com", i))
+                    .withEmail2(String.format("email2.%s@test.com", i))
+                    .withEmail3(String.format("email3.%s@test.com", i)));
+        }
+        return contacts;
+    }
 }
