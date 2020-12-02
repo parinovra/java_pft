51934307b69ec72118d1c74970a9c7a39b18fc0d
parinovra <43321097+parinovra@users.noreply.github.com>
2020-12-03 02:45:10 +0300

    6.11. Протоколирование (лог-файлы)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,13 +1,21 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     protected static ApplicationManager app; //первый тянется из конфига, а второй дефолтный на случай отсутствия в конфиге
 
@@ -28,6 +36,16 @@ public class TestBase {
     @AfterSuite(alwaysRun = true)
     public void tearDown() throws Exception {
         app.stop();
+    }
+
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m) {
+        logger.info("Stop test " + m.getName());
     }
 
     public ApplicationManager getApp() {
