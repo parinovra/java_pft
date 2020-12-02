77ede67b147d06aef029374b49acc6fa8d46b1dd
parinovra <43321097+parinovra@users.noreply.github.com>
2020-12-02 06:42:32 +0300

    6.8. Автоматическое закрытие файлов после использования
    Тесты создания групп/контактов работают

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -53,30 +53,30 @@ public class ContactDataGenerator {
 
     private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
-        Writer writer = new FileWriter(file);
-        for (ContactData contact : contacts) {
-            writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getFirstName(), contact.getLastName(),
-                    contact.getAddress(), contact.getHome(), contact.getMobile(), contact.getWork(),
-                    contact.getEmail(), contact.getEmail2(), contact.getEmail3(), contact.getGroup())); //группу добавить?
+        try (Writer writer = new FileWriter(file)) {
+            for (ContactData contact : contacts) {
+                writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getFirstName(), contact.getLastName(),
+                        contact.getAddress(), contact.getHome(), contact.getMobile(), contact.getWork(),
+                        contact.getEmail(), contact.getEmail2(), contact.getEmail3(), contact.getGroup())); //группу добавить?
+            }
         }
-        writer.close();
     }
 
     private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
         XStream xstream = new XStream();
         xstream.processAnnotations(ContactData.class);
         String xml = xstream.toXML(contacts);
-        Writer writer = new FileWriter(file);
-        writer.write(xml);
-        writer.close();
+        try (Writer writer = new FileWriter(file)) {
+            writer.write(xml);
+        }
     }
 
     private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
         String json = gson.toJson(contacts);
-        Writer writer = new FileWriter(file);
-        writer.write(json);
-        writer.close();
+        try (Writer writer = new FileWriter(file)) {
+            writer.write(json);
+        }
     }
 
     private List<ContactData> generateContacts(int count) {
