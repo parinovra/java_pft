409c7cc29fb3889f7f2be6d5e6f5c58879e12cfa
parinovra <43321097+parinovra@users.noreply.github.com>
2020-12-02 05:24:41 +0300

    6.6 Генерируем тестовые данные групп/контактов в xml
    GroupDataGenerator
    ContactDataGenerator
    Тесты создания групп работают, а тесты контактов стали падать на ContactHelper.java:35
                new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -7,10 +8,13 @@ import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -30,18 +34,22 @@ public class ContactCreationTests extends TestBase {
     @DataProvider
     public Iterator<Object[]> validContacts() throws IOException {
         File photo = new File("src/test/resources/stru.png");
-        List<Object[]> list = new ArrayList<Object[]>();
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+        String xml = "";
         String line = reader.readLine();
         while (line != null) {
-            String[] split = line.split(";");
-            list.add(new Object[] {new ContactData().withFirstName(split[0]).withLastName(split[1])
-                    .withAddress(split[2]).withHome(split[3]).withMobile(split[4]).withWork(split[5])
-                    .withEmail(split[6]).withEmail2(split[7]).withEmail3(split[8]).withGroup("test1").withPhoto(photo)
-            });
+            xml += line;
+//            String[] split = line.split(";");
+//            list.add(new Object[] {new ContactData().withFirstName(split[0]).withLastName(split[1])
+//                    .withAddress(split[2]).withHome(split[3]).withMobile(split[4]).withWork(split[5])
+//                    .withEmail(split[6]).withEmail2(split[7]).withEmail3(split[8]).withGroup("test1").withPhoto(photo)
+//            });
             line = reader.readLine();
         }
-        return list.iterator();
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+        return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
     }
 
     //    @Test(enabled = true)
