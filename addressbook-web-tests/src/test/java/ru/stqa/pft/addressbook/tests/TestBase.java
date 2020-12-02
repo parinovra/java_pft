69b6f87d886f199b60518cabae0e5543218d5b81
parinovra <43321097+parinovra@users.noreply.github.com>
2020-12-03 01:30:40 +0300

    6.10. Описание конфигурации тестового стенда
    Все тесты работают

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -5,9 +5,19 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.io.IOException;
+
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected static ApplicationManager app; //первый тянется из конфига, а второй дефолтный на случай отсутствия в конфиге
+
+    static {
+        try {
+            app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @BeforeSuite(alwaysRun = true)
     public void setUp() throws Exception {
