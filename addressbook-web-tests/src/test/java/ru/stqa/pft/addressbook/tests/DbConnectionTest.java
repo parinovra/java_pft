dad20c118e74fd8b2c4429e36f3239876d296d2e
parinovra <43321097+parinovra@users.noreply.github.com>
2020-12-04 02:30:10 +0300

    7.1. Драйвер базы данных. Получение данных из БД

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTest.java
@@ -0,0 +1,36 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
+
+import java.sql.*;
+
+public class DbConnectionTest {
+
+    @Test
+    public void testDbConnection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=");
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("select group_id, group_name, group_header, group_footer from group_list"); //rs - result set, аналог коллекции, набор строк
+            Groups groups = new Groups();
+            while (rs.next()) {
+                groups.add(new GroupData().withId(rs.getInt("group_id")).withName(rs.getString("group_name"))
+                        .withHeader(rs.getString("group_header"))
+                        .withFooter(rs.getString("group_footer")));
+            }
+            rs.close(); //это значит, читать больше не будем
+            st.close(); //это значит, не будем выполнять никакие запросы
+            conn.close(); //закрыть соединение с БД
+            System.out.println(groups);
+
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+}
