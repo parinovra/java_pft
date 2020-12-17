4ae78887d3858993e775a1b0beda2f2791d4cea4
parinovra <43321097+parinovra@users.noreply.github.com>
2020-12-17 04:56:07 +0300

    4 минуты 8.6
    Работает!

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -1,18 +1,18 @@
 package ru.stqa.pft.mantis.appmanager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class RegistrationHelper {
-
-    private final ApplicationManager app;
-    private WebDriver wd;
+public class RegistrationHelper extends HelperBase {
 
     public RegistrationHelper(ApplicationManager app) {
-        this.app = app;
-        wd = app.getDriver();
+        super(app);
     }
 
     public void start(String username, String email) {
         wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+        type(By.name("username"), username);
+        type(By.name("email"), email);
+        click(By.cssSelector("input[value='Зарегистрироваться']")); //Signup
     }
 }
