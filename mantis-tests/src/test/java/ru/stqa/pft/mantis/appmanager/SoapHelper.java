90eaa2d4fb2db9cadc8f8b12203a3cc22a92b5ba
parinovra <43321097+parinovra@users.noreply.github.com>
2020-12-21 01:52:37 +0300

    9.3. SOAP_ взаимодействие с MantisBT

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/SoapHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/SoapHelper.java
@@ -1,11 +1,11 @@
 package ru.stqa.pft.mantis.appmanager;
 
-import biz.futureware.mantis.rpc.soap.client.MantisConnectLocator;
-import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
-import biz.futureware.mantis.rpc.soap.client.ProjectData;
+import biz.futureware.mantis.rpc.soap.client.*;
+import ru.stqa.pft.mantis.model.Issue;
 import ru.stqa.pft.mantis.model.Project;
 
 import javax.xml.rpc.ServiceException;
+import java.math.BigInteger;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.rmi.RemoteException;
@@ -22,13 +22,37 @@ public class SoapHelper {
     }
 
     public Set<Project> getProjects() throws MalformedURLException, RemoteException, ServiceException {
-        MantisConnectPortType mc = new MantisConnectLocator()
-                .getMantisConnectPort(new URL("http://localhost/mantisbt-2.24.2/api/soap/mantisconnect.php"));
-        ProjectData[] projects = mc.mc_projects_get_user_accessible("administrator", "root"); //массив проектов
+        MantisConnectPortType mc = getMantisConnect();
+        //массив проектов
+        ProjectData[] projects = mc.mc_projects_get_user_accessible("administrator", "root"); //ДЗ: ПЕРЕНЕСТИ СТРОКИ В КОНФИГ. ФАЙЛ
 
         //преобразуем полученные данные в модельные объекты
         return Arrays.asList(projects).stream()
                 .map((p) -> new Project().withId(p.getId().intValue()).withName(p.getName())) //.intValue() для преобразования BigInteger в Int
                 .collect(Collectors.toSet());
+    }
+
+    private MantisConnectPortType getMantisConnect() throws ServiceException, MalformedURLException {
+        MantisConnectPortType mc = new MantisConnectLocator()
+                .getMantisConnectPort(new URL("http://localhost/mantisbt-2.24.2/api/soap/mantisconnect.php")); //ДЗ: ПЕРЕНЕСТИ СТРОКИ В КОНФИГ. ФАЙЛ
+        return mc;
+    }
+
+    public Issue addIssue(Issue issue) throws MalformedURLException, ServiceException, RemoteException {
+        MantisConnectPortType mc = getMantisConnect();
+        String[] categories = mc.mc_project_get_categories("administrator", "root", BigInteger
+                .valueOf(issue.getProject().getId()));
+        IssueData issueData = new IssueData();
+        issueData.setSummary(issue.getSummary());
+        issueData.setDescription(issue.getDescription());
+        issueData.setProject(new ObjectRef(BigInteger
+                .valueOf(issue.getProject().getId()), issue.getProject().getName()));
+        issueData.setCategory(categories[0]); //выбрать первую поавшуюсь категорию
+        BigInteger issueId = mc.mc_issue_add("administrator", "root", issueData);
+        IssueData createdIssueData = mc.mc_issue_get("administrator", "root", issueId);
+        return new Issue().withId(createdIssueData.getId().intValue())
+                .withSummary(createdIssueData.getSummary()).withDescription(createdIssueData.getDescription())
+                .withProject(new Project().withId(createdIssueData.getProject().getId().intValue())
+                                          .withName(createdIssueData.getProject().getName()));
     }
 }
