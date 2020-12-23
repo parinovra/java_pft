c5d8ab208dbc9e57d0934705284252ad09148edd
parinovra <43321097+parinovra@users.noreply.github.com>
2020-12-24 00:10:51 +0300

    Конец 9.4. - RestTests НЕ взлетает
    Падает на сравнении

---
+++ rest-sample/src/test/java/ru/stqa/pft/rest/Issue.java
@@ -0,0 +1,55 @@
+package ru.stqa.pft.rest;
+
+public class Issue {
+
+    private int id;
+    private String subject;
+    private String Description;
+
+    public int getId() {
+        return id;
+    }
+
+    public Issue withId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public Issue withSubject(String subject) {
+        this.subject = subject;
+        return this;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public Issue withDescription(String description) {
+        Description = description;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Issue issue = (Issue) o;
+
+        if (id != issue.id) return false;
+        if (subject != null ? !subject.equals(issue.subject) : issue.subject != null) return false;
+        return Description != null ? Description.equals(issue.Description) : issue.Description == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (subject != null ? subject.hashCode() : 0);
+        result = 31 * result + (Description != null ? Description.hashCode() : 0);
+        return result;
+    }
+}
