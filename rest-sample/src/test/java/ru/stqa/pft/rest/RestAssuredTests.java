da6c4158636b9f48e408b0a481f88d645497dd7e
parinovra <43321097+parinovra@users.noreply.github.com>
2020-12-25 20:31:29 +0300

    9.5. RestAssured_ использовать или нет
    RestAssuredTests тоже не взлетел

--- rest-sample/src/test/java/ru/stqa/pft/rest/RestAssuredTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestAssuredTests.java
@@ -4,9 +4,8 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
-import org.apache.http.client.fluent.Executor;
-import org.apache.http.client.fluent.Request;
-import org.apache.http.message.BasicNameValuePair;
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -14,7 +13,12 @@ import java.util.Set;
 
 import static org.testng.Assert.assertEquals;
 
-public class RestTests {
+public class RestAssuredTests {
+
+    @BeforeClass
+    public void init() {
+        RestAssured.authentication = RestAssured.basic("288f44776e7bec4bf44fdfeb1e646490", "");
+    }
 
     @Test
     public void testCreateIssue() throws IOException {
@@ -27,28 +31,17 @@ public class RestTests {
     }
 
     private Set<Issue> getIssues() throws IOException {
-        String json = getExecutor().execute(Request.Get("https://bugify.stqa.ru/api/issues.json"))
-                .returnContent().asString();
-        //вместо этого
-//        JsonElement parsed = new JsonParser().parse(json);
-        //это
+        String json = RestAssured.get("https://bugify.stqa.ru/api/issues.json").asString();
         JsonElement parsed = JsonParser.parseString(json);
         JsonElement issues = parsed.getAsJsonObject().get("issues");
         return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
     }
 
-    private Executor getExecutor() {
-        return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
-    }
-
     private int createIssue(Issue newIssue) throws IOException {
-        String json = getExecutor().execute(Request.Post("https://bugify.stqa.ru/api/issues.json")
-                .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
-                        new BasicNameValuePair("description", newIssue.getDescription())))
-                .returnContent().asString();
-        //вместо этого
-//        JsonElement parsed = new JsonParser().parse(json);
-        //это
+        String json = RestAssured.given()
+                .param("subject", newIssue.getSubject())
+                .param("description", newIssue.getDescription())
+                .post("https://bugify.stqa.ru/api/issues.json").asString();
         JsonElement parsed = JsonParser.parseString(json);
         return parsed.getAsJsonObject().get("issue_id").getAsInt(); //id созданного баг-репорта
     }
